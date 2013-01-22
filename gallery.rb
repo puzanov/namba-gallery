686993b2a5010b04890a52e9d6242674eeea2333
Oleg Puzanov <puzanov@gmail.com>
2013-01-22 14:09:42 +0600

    Готова автоподгрузка фоток

--- gallery.rb
+++ gallery.rb
@@ -8,8 +8,15 @@ get '/favicon.ico' do
   '1'
 end
 
+get '/:id/:page' do |id, page|
+  content_type :json
+  @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=25&param=#{id}&page=#{page}").read)
+  @photos['elements'].to_json
+end
+
 get '/:id' do |id|
-  @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=15&param=#{id}").read)
+  @id = id
+  @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=25&param=#{id}").read)
   erb :photos
 end
 

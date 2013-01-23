6d2da86d4424effff8332282d8883f16ca4868d9
Oleg Puzanov <puzanov@gmail.com>
2013-01-23 11:16:10 +0600

    Добавил реальный фавикон и лого Намбы

--- gallery.rb
+++ gallery.rb
@@ -4,10 +4,6 @@ require "sinatra"
 require "json"
 require "open-uri"
 
-get '/favicon.ico' do
-  '1'
-end
-
 get '/:id/:page' do |id, page|
   content_type :json
   @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=25&param=#{id}&page=#{page}").read)

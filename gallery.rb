3417b065597404e64766c995d3d779b33f7a408e
Oleg Puzanov <puzanov@gmail.com>
2013-01-25 15:28:15 +0600

    Сделал главную

--- gallery.rb
+++ gallery.rb
@@ -7,18 +7,28 @@ require "open-uri"
 set :environment, :production
 set :server, %w[thin]
 
-get '/:id/:page' do |id, page|
+get '/:page' do |page|
+  content_type :json
+  @main = true
+  @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=category_albums&category=super&page=1&sort=desc&page=#{page}").read)
+  @photos['albums'].to_json
+end
+
+get '/' do
+  @main = true
+  @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=category_albums&category=super&page=1&sort=desc").read)
+  erb :photos
+end
+
+get '/album/:id/:page' do |id, page|
   content_type :json
   @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=25&param=#{id}&page=#{page}").read)
   @photos['elements'].to_json
 end
 
-get '/:id' do |id|
+get '/album/:id' do |id|
+  @main = false
   @id = id
   @photos = JSON.parse(open("http://namba.kg/api/?service=photo&action=elements_list&type=album&limit=25&param=#{id}").read)
   erb :photos
-end
-
-get '/' do
-  'Main page'
 end
